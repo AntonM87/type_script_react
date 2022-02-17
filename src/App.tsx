412eb02568b2b722f371cf767040f4d779032d82
Anton <anton.m.87@mail.ru>
2022-02-17 08:26:56 +0500

    пропрсы тайп скрипт

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Card, CardVariant} from "./components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Card onClick={(message:string)=> console.log(message)} width='200px' height='200px' variant={CardVariant.outlined}>
+                <button>Кнопка</button>
+            </Card>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,30 @@
+import React, {FunctionComponent, ReactChild, ReactNode, useState} from 'react';
+
+enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
+
+interface CardProps {
+    width: string,
+    height: string,
+    variant: CardVariant,
+    onClick: (message: string) => void,
+}
+
+const Card: FunctionComponent<CardProps> = ({width, height, variant, children, onClick}) => {
+    const [state, setState] = useState('Hello world');
+    return (
+        <div style={{
+            width, height,
+            border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+            background: variant === CardVariant.outlined ? 'lightgray' : 'none'
+        }}
+             onClick={() => onClick(state)}
+        >
+            {children}
+        </div>
+    );
+};
+
+export {Card, CardVariant};
